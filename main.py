f7114c49b26d7615a484ab0f9d265784b5357242
IPad4610sabu-2 <aihfhauu@gmail.com>
2024-12-24 20:27:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -221,6 +221,7 @@ def apicommentsrequest(url):
 video_apis = [
     r"https://invidious.jing.rocks/",
     r"https://invidious.nerdvpn.de/",
+    r"https://inv.nadeko.net/",
    r"https://script.google.com/macros/s/AKfycbw43HTKJe0khOM3h5lrRbWw2OUONcbQCsnSry7F6c_1bPdtxVjTUotm1_XY2KfqMLWT/exec?videoId="
 ]
 
